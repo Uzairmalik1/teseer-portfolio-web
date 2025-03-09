8542b859bd459c89312de8f542eb3678a7c7e680
Uzair Malik <uzairullah397@gmail.com>
2025-03-10 00:05:28 +0500

    some errors removed

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
@@ -9,27 +8,9 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import { client } from "@/sanity/lib/client";
 import { BLOG_QUERIES } from "@/sanity/lib/queries";
-// import { blogs } from "@/utils/data/blogs"
-
-// async function getData() {
-//   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
-
-//   const data = await res.json();
-
-//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-//   return filtered;
-// };
 
 export default async function Home() {
-  // const blogs = await getData();
   const blogs = await client.fetch(BLOG_QUERIES);
-
-  console.log("blog post: ", blogs)
 
   return (
     <div suppressHydrationWarning >
