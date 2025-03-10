c55c70b877e84f9f9cdaa5de3484aee5f90bdb5a
uzair malik <96469036+Uzairmalik1@users.noreply.github.com>
2025-03-11 00:23:27 +0500

    Update page.js uncommented project section

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default async function Home() {
    <AboutSection />
       {/*  <Experience />*/}
   <Skills />
-     {/* <Projects />*/}
+     <Projects />
      {/* <Education />*/}
      {/* <Blog blogs={blogs} />*/}
        {/* <ContactSection /> */}
