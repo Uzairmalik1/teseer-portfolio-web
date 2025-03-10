1c796b5561afbbcb1a363eb19ad725f237d80dc9
uzair malik <96469036+Uzairmalik1@users.noreply.github.com>
2025-03-10 23:16:38 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,12 +15,12 @@ export default async function Home() {
   return (
     <div >
       <HeroSection /> 
-    <AboutSection />
-      <Experience />
-     <Skills />
-      <Projects />
-      <Education />
-      <Blog blogs={blogs} />
+   {/* <AboutSection />*/}
+     {/* <Experience />*/}
+   {/*  <Skills />*/}
+     {/* <Projects />*/}
+     {/* <Education />*/}
+     {/* <Blog blogs={blogs} />*/}
        {/* <ContactSection /> */}
     </div>
   )
